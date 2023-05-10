1d5df6fa91a93ac4383a7581b358847059758fa8
Latisha Bridges <msbridges3368@gmail.com>
2023-05-10 13:32:11 -0400

    added dogs

--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
-const btn = document.querySelector('button')
+const catbtn = document.querySelector('#cat')
+const dogbtn = document.querySelector('#dog')
 
-const clickHandler = () => alert('The cat name is: Rascal')
 
-btn.addEventListener('click', clickHandler)
+const catHandler = () => alert('The cat name is: Rascal')
+const dogHandler = () => alert('The dog name is: Doug')
+
+catbtn.addEventListener('click', catHandler)
+dogbtn.addEventListener('click', dogHandler)
