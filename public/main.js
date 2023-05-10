4c8ad9c8ec86c734ad33bc2d3036fd252c79e41a
Latisha Bridges <msbridges3368@gmail.com>
2023-05-10 13:42:50 -0400

    updated variables

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
-const catbtn = document.querySelector('#cat')
-const dogbtn = document.querySelector('#dog')
+const catBtn = document.querySelector('#cat')
+const dogBtn = document.querySelector('#dog')
 
 
 const catHandler = () => alert('The cat name is: Rascal')
 const dogHandler = () => alert('The dog name is: Doug')
 
-catbtn.addEventListener('click', catHandler)
-dogbtn.addEventListener('click', dogHandler)
+catBtn.addEventListener('click', catHandler)
+dogBtn.addEventListener('click', dogHandler)
